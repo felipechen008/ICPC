44c13baa40ca0565bfd10f58b98749836c8858c4
felipechen008 <48738093+felipechen008@users.noreply.github.com>
2020-12-15 23:07:42 -0300

    Update tarjanSCC.cpp

--- code/graphs/tarjanSCC.cpp
+++ code/graphs/tarjanSCC.cpp
@@ -33,7 +33,7 @@ void dfs( int u ) {
 void tarjan() {
 	memset( scc, -1, sizeof scc );
 	memset( in, -1, sizeof in );
-	for( int i = 1 ; i <= n ; ++i ) if (scc[i] == -1) dfs(i);
+	for( int i = 0 ; i < n ; ++i ) if (scc[i] == -1) dfs(i);
 }
 
 // Mount condensed graph
